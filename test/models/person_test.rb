161c9000c7ac96735a903deb3b630904097888db
HarrisonScarfone <hscarfone77@gmail.com>
2021-12-24 15:49:43 -0500

    set up model testing, shrink navbar form titles

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+    test '.save saves a person with valid parameters' do
+    end
+
+    test '.save does not save a person without a name' do
+    end  
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test '.save saves a project with valid parameters' do
+  end
+
+  test '.save does not save a project without a title' do
+  end
+
+  test '.save does not save a project without an abstract' do
+  end
+
+  test '.save does not save a project without a valid youtube video link' do
+  end
+
+  test '.save does not save a project without a valid department' do
+  end
 end
