dfc266e93582242e25e29ec4441bf2e8aacac625
HarrisonScarfone <hscarfone77@gmail.com>
2022-01-14 00:50:13 -0500

    tests submission_controller, submission_package

--- test/controllers/submissions_controller_test.rb
+++ test/controllers/submissions_controller_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
 
 class SubmissionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#create renders a form successfully' do
+    get '/submissionCreate'
+
+    assert_response :success
+  end
+
+  test '#create_download creates a download with valid form parameters' do
+    params = {
+      'title': "Some Rando Project",
+      'abstract': "this is a test abstract of a project.  is it about a project where we did a project and had fun 
+          doing the project! this is some more text just to see how it handles a longer paragraph.  did i mention 
+          that it was about a project that we made that was a project because we had to make a project? anyways 
+          this is my project",
+      'videoLink': "https://www.youtube.com/embed/a46Ako2Y970",
+      'department': 'electrical',
+      'year': 2021,
+      'groupMember1': 'Bob McFred',
+      'groupMember2': 'Linda McLisa'
+  }
+
+    get '/createDownload', params: params
+
+    assert_response :ok
+  end
+
+  test '#create_download triggers a redirect with invalid form parameters' do
+    params = {
+      'title': "Some Rando Project",
+      'abstract': "this is a test abstract of a project.  is it about a project where we did a project and had fun 
+          doing the project! this is some more text just to see how it handles a longer paragraph.  did i mention 
+          that it was about a project that we made that was a project because we had to make a project? anyways 
+          this is my project",
+      'videoLink': "NOT A VALID LINK",
+      'department': 'electrical',
+      'year': 2021,
+      'groupMember1': 'Bob McFred',
+      'groupMember2': 'Linda McLisa'
+  }
+
+    get '/createDownload', params: params
+
+    assert_response :found
+  end
 end
---
+++ test/public/submission/submission_package_test.rb
@@ -0,0 +1,126 @@
+module Submission
+  class SubmissionPackageTest < ActiveSupport::TestCase
+    setup do
+      @action = 'create_download'
+
+      @project_as_hash = {
+        department: 'electrical',
+        video_link: "https://www.youtube.com/embed/a46Ako2Y970",
+        title: 'A' * 10,
+        abstract: 'a' * 100,
+        year: 2022,
+      }
+      
+      @people_as_array = [
+        'Bob McBobbyBob',
+        'Cindy McCindyCind'
+      ]
+    end
+
+    test '.valid_submission? returns true for a valid submission package' do
+      submission_package = Submission::SubmissionPackage.new(
+        action: @action,
+        project_as_hash: @project_as_hash,
+        people_as_array: @people_as_array
+      )
+
+      assert submission_package.valid_submission?
+    end
+
+    test '.valid_submission? returns false without the create_download action' do
+      submission_package = Submission::SubmissionPackage.new(
+        action: 'invalid_action',
+        project_as_hash: @project_as_hash,
+        people_as_array: @people_as_array
+      )
+
+      assert !submission_package.valid_submission?
+    end
+
+    test '.valid_submission? returns false when no valid project is found' do
+      submission_package = Submission::SubmissionPackage.new(
+        action: @action,
+        project_as_hash: {},
+        people_as_array: @people_as_array
+      )
+
+      assert !submission_package.valid_submission?
+    end
+
+    test '.valid_submission? returns false when no people are found' do
+      submission_package = Submission::SubmissionPackage.new(
+        action: @action,
+        project_as_hash: @project_as_hash,
+        people_as_array: []
+      )
+
+      assert !submission_package.valid_submission?
+    end
+
+    test '.error_messages_as_string returns a single error message as a string' do
+      submission_package = Submission::SubmissionPackage.new(
+        action: {},
+        project_as_hash: @project_as_hash,
+        people_as_array: @people_as_array
+      )
+
+      assert submission_package.error_messages_as_string.instance_of?(String)
+      assert_equal submission_package.error_messages_as_string, "Incorrect action detected."
+    end
+
+    test '.error_messages_as_string returns a multiple error messages as a string' do
+      submission_package = Submission::SubmissionPackage.new(
+        action: @action,
+        project_as_hash: {},
+        people_as_array: @people_as_array
+      )
+
+      expected_result = [
+        "Video link can't be blank, Video link is not a valid youtube video embed link, T",
+        "itle can't be blank, Title must be at least 10 characters, Abstract can't be blan",
+        "k, Abstract must be at least 100 characters, Year can't be blank, Department ca",
+        "n't be blank"
+      ].join('')
+
+      assert submission_package.error_messages_as_string.instance_of?(String)
+      assert_equal submission_package.error_messages_as_string, expected_result
+    end
+
+    test '.error_messages_as_string returns nothing when submission_package is valid' do
+      submission_package = Submission::SubmissionPackage.new(
+        action: @action,
+        project_as_hash: @project_as_hash,
+        people_as_array: @people_as_array
+      )
+
+      assert submission_package.error_messages_as_string.blank?
+    end
+
+    test '.generate_json converts a submission_package to a json and returns it' do
+      submission_package = Submission::SubmissionPackage.new(
+        action: @action,
+        project_as_hash: @project_as_hash,
+        people_as_array: @people_as_array
+      )
+
+      expected_result = [
+        "{\"department\":\"electrical\",\"year\":2022,\"title\":\"AAAAAAAAAA\",",
+        "\"abstract\":\"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        "aaaaaaaaaaaaaaaaaaaaaaaaaa\",\"video_link\":\"https://www.youtube.com/embed/a46Ako2Y970\",",
+        "\"group_members\":[\"Bob McBobbyBob\",\"Cindy McCindyCind\"]}"
+      ].join('')
+
+      assert_equal submission_package.generate_json, expected_result
+    end
+
+    test '.generate_json does not generate a json if the submission package is not valid' do
+      submission_package = Submission::SubmissionPackage.new(
+        action: @action,
+        project_as_hash: {},
+        people_as_array: @people_as_array
+      )
+
+      assert_nil submission_package.generate_json
+    end
+  end
+end
